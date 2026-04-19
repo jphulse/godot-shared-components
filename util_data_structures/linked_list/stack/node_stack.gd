ebb110eb8cf925aa0f9eb487e53694e985e6605d
jphulse <hulsej281@gmail.com>
2026-04-19 00:27:10 -0400

    Added NodeQueue and NodeStack

---
+++ util_data_structures/linked_list/stack/node_stack.gd
@@ -0,0 +1,29 @@
+class_name NodeStack
+extends RefCounted
+
+var _list := LinkedList.new()
+
+## Pushes a node to the top of the stack
+func push(node: Node) -> void:
+	assert(node != null)
+	_list.push_back(node)
+
+## Pops the node off the top of the stack
+func pop() -> Node:
+	return _list.pop_back()
+
+## Returns the node on the top of the stack but does not remove it
+func peek() -> Node:
+	return _list.back()
+
+## Returns true if the stack is empty, false otherwise
+func is_empty() -> bool:
+	return _list.is_empty()
+
+## Returns the number of nodes in the NodeStack
+func size() -> int:
+	return _list.size()
+
+## Resets the stack
+func clear() -> void:
+	_list.clear()
