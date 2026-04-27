a89d3ba845d85ba8d0ddbe430062d4aac0a61b7d
jphulse <hulsej281@gmail.com>
2026-04-26 23:16:04 -0400

    Modified plugin to make it better for adding/removing autoload from the game for the save manager

--- plugin.gd
+++ plugin.gd
@@ -1,33 +1,62 @@
 @tool
 extends EditorPlugin
 
-const SAVE_MANAGER_NAME : String = "SaveManager"
-const SAVE_MANAGER_PATH : String = "res://addons/jeremy_components/autoloads/save_manager/save_manager.gd"
+## The autoload singleton name used by this plugin.
+const SAVE_MANAGER_NAME: String = "SaveManager"
+
+## The script path for this plugin's SaveManager autoload.
+const SAVE_MANAGER_PATH: String = "res://addons/jeremy_components/autoloads/save_manager/save_manager.gd"
+
+## The ProjectSettings key used by Godot for this autoload.
+const SAVE_MANAGER_SETTING_NAME: String = "autoload/%s" % SAVE_MANAGER_NAME
 
 
-func _enter_tree() -> void:
-	var setting_name := "autoload/%s" % SAVE_MANAGER_NAME
-
-	if not ProjectSettings.has_setting(setting_name):
+## Adds the SaveManager autoload when the plugin is enabled.
+func _enable_plugin() -> void:
+	if not ProjectSettings.has_setting(SAVE_MANAGER_SETTING_NAME):
 		add_autoload_singleton(SAVE_MANAGER_NAME, SAVE_MANAGER_PATH)
+		ProjectSettings.save()
 		return
 
-	var existing_path: String = str(ProjectSettings.get_setting(setting_name))
+	var existing_value: String = str(ProjectSettings.get_setting(SAVE_MANAGER_SETTING_NAME))
+	var existing_path: String = _normalize_autoload_path(existing_value)
 
-	if existing_path != "*%s" % SAVE_MANAGER_PATH and existing_path != SAVE_MANAGER_PATH:
+	if existing_path == SAVE_MANAGER_PATH:
+		return
+
+	push_warning(
+		"Could not add SaveManager autoload because an autoload named SaveManager already exists at: %s"
+		% existing_value
+	)
+
+
+## Removes the SaveManager autoload when the plugin is disabled.
+func _disable_plugin() -> void:
+	if not ProjectSettings.has_setting(SAVE_MANAGER_SETTING_NAME):
+		return
+
+	var existing_value: String = str(ProjectSettings.get_setting(SAVE_MANAGER_SETTING_NAME))
+	var existing_path: String = _normalize_autoload_path(existing_value)
+
+	if existing_path != SAVE_MANAGER_PATH:
 		push_warning(
-			"Could not add SaveManager autoload because an autoload named SaveManager already exists at: %s"
-			% existing_path
+			"Not removing SaveManager autoload because it does not point to this plugin. Existing value: %s"
+			% existing_value
 		)
-
-
-func _exit_tree() -> void:
-	var setting_name := "autoload/%s" % SAVE_MANAGER_NAME
-
-	if not ProjectSettings.has_setting(setting_name):
 		return
 
-	var existing_path: String = str(ProjectSettings.get_setting(setting_name))
+	remove_autoload_singleton(SAVE_MANAGER_NAME)
+	ProjectSettings.save()
 
-	if existing_path == "*%s" % SAVE_MANAGER_PATH or existing_path == SAVE_MANAGER_PATH:
-		remove_autoload_singleton(SAVE_MANAGER_NAME)
+
+## Converts an autoload ProjectSettings value into a comparable res:// path.
+func _normalize_autoload_path(value: String) -> String:
+	var cleaned_value: String = value.trim_prefix("*")
+
+	if cleaned_value.begins_with("uid://"):
+		var resolved_path: String = ResourceUID.ensure_path(cleaned_value)
+
+		if not resolved_path.is_empty():
+			return resolved_path
+
+	return cleaned_value
